261e494c0ecedf49d26195e5db6004de51e46e7c
Schmedake <schmedak15@up.edu>
2015-09-26 19:58:30 -0700

    Layouts Completed

--- app/src/main/java/com/example/schmedake/fantasysoccer/MainActivity.java
+++ app/src/main/java/com/example/schmedake/fantasysoccer/MainActivity.java
@@ -4,8 +4,36 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Create team TextView variables
+    protected TextView teamNameField;
+    protected TextView numPlayersField;
+    protected TextView numWinsField;
+    protected TextView numLossesField;
+    protected TextView numDrawsField;
+
+    // Create player TextView variables
+    protected TextView playerNameField;
+    protected TextView teamField;
+    protected TextView numberField;
+    protected TextView goalsField;
+    protected TextView assistsField;
+    protected TextView savesField;
+    protected TextView positionField;
+    protected TextView skillField;
+    protected TextView yellowField;
+    protected TextView redField;
+    protected TextView tacklesField;
+
+    // Create button variables
+    protected Button editTeamButton;
+    protected Button playGameButton;
+
+    // Create
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,25 +41,4 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
--- app/src/main/java/com/example/schmedake/fantasysoccer/PlayerEditActivity.java
+++ app/src/main/java/com/example/schmedake/fantasysoccer/PlayerEditActivity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class PlayerEditActivity extends AppCompatActivity {
+public class PlayerEditActivity extends MainActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,25 +13,4 @@ public class PlayerEditActivity extends AppCompatActivity {
         setContentView(R.layout.activity_player_edit);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_player_edit, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
